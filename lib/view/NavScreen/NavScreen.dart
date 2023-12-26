8118e222697661d7f85ec1329d547278814bfc2e
itsmedona <donashaji254@gmail.com>
2023-12-26 22:48:59 +0530

    constants added

--- lib/view/NavScreen/NavScreen.dart
+++ lib/view/NavScreen/NavScreen.dart
@@ -2,7 +2,6 @@ import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
 import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_partner/view/FavoritesPage/FavoritesPage.dart';
-import 'package:travel_partner/view/HomeAppBar/HomeAppBar.dart';
 import 'package:travel_partner/view/MapsPage/MapsPage.dart';
 import 'package:travel_partner/view/MyHomePage/MyHomePage.dart';
 import 'package:travel_partner/view/SearchPage/SearchPage.dart';
