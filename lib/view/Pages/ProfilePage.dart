2ad62e2f18098f19bfc7aadab3cba208e8d8d88d
itsmedona <donashaji254@gmail.com>
2024-01-03 00:34:10 +0530

    Hive implemented

---
+++ lib/view/Pages/ProfilePage.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage  extends ConsumerWidget {
+  final PageController _pageController = PageController();
+  final ValueNotifier<int> _currentPage = ValueNotifier<int>(0);
+  
+   @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(tripListNotifierProvider.notifier).loadTrips();
+    _pageController.addListener(() {
+      _currentPage.value = _pageController.page!.round();
+    });return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0,toolbarHeight: 100,
+        backgroundColor: Colors.white,
+        title: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+             Text(
+              'Hi Fabrice 👋',
+              textAlign: TextAlign.start,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+            ),
+            Text(
+              'Travelling Today ?',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+            ),
+          ],
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100.0),
+              child: Image.network(
+              profilPic,
+                fit: BoxFit.cover,
+                height: 60,
+                width: 60,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        children: [
+          MyTripsScreen(),
+          AddTripScreen(),
+          Text("Maps"),
+        ],
+      ),
+      bottomNavigationBar: ValueListenableBuilder<int>(
+        valueListenable: _currentPage,
+        builder: (context, pageIndex, child) {
+          return BottomNavigationBar(
+            currentIndex: pageIndex, // Set the current page index here
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                label: 'My Trips',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add),
+                label: 'Add Trip',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.map),
+                label: 'Maps',
+              ),
+            ],
+            onTap: (index) {
+              _pageController.jumpToPage(index);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+
